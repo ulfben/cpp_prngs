24449b2ab44aff83e7708973c9b5b382e94716fc
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2024-08-13 15:04:34 +0200

    Update SmallPRNG_32.h
    Make SmallPRNG_32 satisfy UniformRandomBitGenerator

--- SmallPRNG_32.h
+++ SmallPRNG_32.h
@@ -8,10 +8,12 @@
 A C++ 32-bit two-rotate implementation of the famous Jenkins Small Fast PRNG.
 Original public domain C-code and writeup by Bob Jenkins https://burtleburtle.net/bob/rand/smallprng.html
 C++ implementation by Ulf Benjaminsson (ulfbenjaminsson.com), also placed in the public domain. Use freely!
+
+Satisfies 'UniformRandomBitGenerator', meaning it works well with std::shuffle, std::sample, most of the std::*_distribution-classes, etc.
 */
 class SmallPRNG
  {
-    using u32 = uint32_t;
+    using u32 = uint32_t;    
     u32 a;
     u32 b;
     u32 c;
@@ -22,6 +24,8 @@ class SmallPRNG
     }
 
 public:
+    using result_type = u32;
+
     constexpr SmallPRNG(u32 seed) noexcept : a(0xf1ea5eed), b(seed), c(seed), d(seed) {        
         // warmup: run the generator a couple of cycles to mix the state thoroughly
         for (auto i = 0; i < 20; ++i) { 
@@ -30,10 +34,14 @@ public:
     }
     constexpr SmallPRNG(std::span<const u32, 4> state) noexcept : a(state[0]), b(state[1]), c(state[2]), d(state[3]) {}
 
-    constexpr u32 max() noexcept{
+    static constexpr result_type max() noexcept{
         return std::numeric_limits<u32>::max();
     }
-  
+    
+    static constexpr result_type min() noexcept {
+        return std::numeric_limits<u32>::lowest();
+    }
+
   	constexpr bool coinToss() noexcept{
         return next() & 1; //checks the least significant bit
     }
@@ -58,11 +66,11 @@ public:
 
     template<typename T = float>
     constexpr T unit_range() noexcept {
-        static_assert(std::is_floating_point_v<T>, "raunit_range can only be used with floating point types.");
+        static_assert(std::is_floating_point_v<T>, "SmallPRNG::unit_range can only be used with floating point types.");
         return static_cast<T>(2.0) * normalized<T>() - static_cast<T>(1.0);
     }
 
-    constexpr u32 next() noexcept {   
+    constexpr result_type next() noexcept {
         const u32 e = a - rot(b, 27); 
         a = b ^ rot(c, 17); 
         b = c + d;
@@ -71,11 +79,15 @@ public:
         return d;
     }
 
+    constexpr result_type operator()() noexcept {
+        return next();
+    }
+
     template<typename T = float>
     constexpr T next_gaussian(T mean, T stddev) noexcept {
-        static_assert(std::is_floating_point_v<T>, "next_guassian can only be used with a floating point type");
+        static_assert(std::is_floating_point_v<T>, "SmallPRNG::next_guassian can only be used with a floating point type");
         static bool hasSpare = false;
-        static T spare;
+        static T spare{};
 
         if (hasSpare) {
             hasSpare = false;
@@ -101,20 +113,23 @@ public:
         *this = SmallPRNG(s);
     }
 };
-//sample usage:
-/*
+
+/*sample usage:
 int main() {
-    SmallPRNG rand(223456721);
+    SmallPRNG rand(223456322);
 
     [[maybe_unused]] auto random_unsigned = rand.between(10u, 50u); 
     [[maybe_unused]] int random_int = rand.between(-10, 10); 
     [[maybe_unused]] float random_normalized = rand.normalized(); //0.0 - 1.0
     [[maybe_unused]] float ndc = rand.unit_range();  //-1.0 - +1.0
-    [[maybe_unused]] auto gaus = rand.next_gaussian(70.0f, 10.0f);
+    //[[maybe_unused]] auto gaus = rand.next_gaussian(70.0f, 10.0f);
     [[maybe_unused]] double random_double = rand.between(1.0, 5.0); 
 
     auto state = rand.get_state();
     rand.set_state(state);
+
+    std::array<int, 10> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::shuffle(data.begin(), data.end(), rand);
 
     return random_unsigned;
 }*/
